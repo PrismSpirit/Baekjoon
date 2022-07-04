c3d412f12f3202417e8c4dc7d2f23e00ae677278
PrismSpirit <yeolkeut@gmail.com>
2022-07-04 16:49:55 +0900

    Solve [18108]1998년생인 내가 태국에서는 2541년생?!

---
+++ Baekjoon/입출력과 사칙연산/[18108]1998년생인 내가 태국에서는 2541년생?!.swift
@@ -0,0 +1,13 @@
+//
+//  [18108]1998년생인 내가 태국에서는 2541년생?!.swift
+//  Baekjoon
+//  https://www.acmicpc.net/problem/18108
+//
+//  Created by Jaehun Lee on 2022/07/04.
+//
+
+import Foundation
+
+var input = Int(readLine()!)!
+
+print(input - 543)
