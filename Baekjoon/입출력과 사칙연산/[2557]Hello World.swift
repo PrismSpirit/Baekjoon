c4d3910bb386ace0e95fc2f49a714e4133de09ad
PrismSpirit <yeolkeut@gmail.com>
2022-07-04 16:10:42 +0900

    Solve [2557]Hello World

---
+++ Baekjoon/입출력과 사칙연산/[2557]Hello World.swift
@@ -0,0 +1,11 @@
+//
+//  [2557]Hello World.swift
+//  Baekjoon
+//  https://www.acmicpc.net/problem/2557
+//
+//  Created by Jaehun Lee on 2022/07/04.
+//
+
+import Foundation
+
+print("Hello World!")
