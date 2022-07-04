d986d657f05a1c193d4422a3a81c52bc51354693
PrismSpirit <yeolkeut@gmail.com>
2022-07-04 19:28:33 +0900

    Solve [2741]N 찍기

---
+++ Baekjoon/반복문/[2741]N 찍기.swift
@@ -0,0 +1,15 @@
+//
+//  [2741]N 찍기.swift
+//  Baekjoon
+//  https://www.acmicpc.net/problem/2741
+//
+//  Created by Jaehun Lee on 2022/07/04.
+//
+
+import Foundation
+
+var n = Int(readLine()!)!
+
+for i in 1...n {
+    print(i)
+}
