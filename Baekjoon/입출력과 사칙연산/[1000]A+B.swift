d47fbf5e002dd6678c66de3d0e505c8bfd68b412
PrismSpirit <yeolkeut@gmail.com>
2022-07-04 16:24:17 +0900

    Solve [1000]A+B

---
+++ Baekjoon/입출력과 사칙연산/[1000]A+B.swift
@@ -0,0 +1,13 @@
+//
+//  [1000]A+B.swift
+//  Baekjoon
+//  https://www.acmicpc.net/problem/1000
+//
+//  Created by Jaehun Lee on 2022/07/04.
+//
+
+import Foundation
+
+var input = readLine()!.components(separatedBy: " ").map { Int($0)! }
+
+print(input[0] + input[1])
