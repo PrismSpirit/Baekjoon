9df6d42222c2df9a6f79f8834e08b5476152493b
PrismSpirit <yeolkeut@gmail.com>
2022-07-04 16:19:20 +0900

    Solve [10172]개

---
+++ Baekjoon/입출력과 사칙연산/[10172]개.swift
@@ -0,0 +1,15 @@
+//
+//  [10172]개.swift
+//  Baekjoon
+//  https://www.acmicpc.net/problem/10172
+//
+//  Created by Jaehun Lee on 2022/07/04.
+//
+
+import Foundation
+
+print("|\\_/|")
+print("|q p|   /}")
+print("( 0 )\"\"\"\\")
+print("|\"^\"`    |")
+print("||_/=\\\\__|")
