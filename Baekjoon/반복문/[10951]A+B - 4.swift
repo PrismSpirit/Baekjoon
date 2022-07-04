722842abc97999078bf7a83637285c25cbd93564
PrismSpirit <yeolkeut@gmail.com>
2022-07-04 19:53:02 +0900

    Solve [10951]A+B - 4

---
+++ Baekjoon/반복문/[10951]A+B - 4.swift
@@ -0,0 +1,15 @@
+//
+//  [10951]A+B - 4.swift
+//  Baekjoon
+//  https://www.acmicpc.net/problem/10951
+//
+//  Created by Jaehun Lee on 2022/07/04.
+//
+
+import Foundation
+
+while let line = readLine() {
+    let input = line!.components(separatedBy: " ")
+    
+    print(input[0] + input[1])
+}
