3dd86af7bc8276d0d14e8737f697852418c46f7f
PrismSpirit <yeolkeut@gmail.com>
2022-07-04 18:31:37 +0900

    Solve [2739]구구단

---
+++ Baekjoon/반복문/[2739]구구단.swift
@@ -0,0 +1,15 @@
+//
+//  [2739]구구단.swift
+//  Baekjoon
+//  https://www.acmicpc.net/problem/2739
+//
+//  Created by Jaehun Lee on 2022/07/04.
+//
+
+import Foundation
+
+var input = Int(readLine()!)!
+
+for i in 1...9 {
+    print("\(input) * \(i) = \(input * i)")
+}
