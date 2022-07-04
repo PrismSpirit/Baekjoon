eac767f50c436ccf8f589602ebf63d831f663ff8
PrismSpirit <yeolkeut@gmail.com>
2022-07-04 16:13:11 +0900

    Solve [10718]We love kriii

---
+++ Baekjoon/입출력과 사칙연산/[10718]We love kriii.swift
@@ -0,0 +1,12 @@
+//
+//  [10718]We love kriii.swift
+//  Baekjoon
+//  https://www.acmicpc.net/problem/10718
+//
+//  Created by Jaehun Lee on 2022/07/04.
+//
+
+import Foundation
+
+print("강한친구 대한육군")
+print("강한친구 대한육군")
