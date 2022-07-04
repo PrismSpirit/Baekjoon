44cf3b12d97fd2397effadb30f5d4344b93d26e6
PrismSpirit <yeolkeut@gmail.com>
2022-07-04 16:35:25 +0900

    Solve [10998]AxB

---
+++ Baekjoon/입출력과 사칙연산/[10998]AxB.swift
@@ -0,0 +1,13 @@
+//
+//  [10998]AxB.swift
+//  Baekjoon
+//  https://www.acmicpc.net/problem/10998
+//
+//  Created by Jaehun Lee on 2022/07/04.
+//
+
+import Foundation
+
+var input = readLine()!.components(separatedBy: " ").map { Int($0)! }
+
+print(input[0] * input[1])
