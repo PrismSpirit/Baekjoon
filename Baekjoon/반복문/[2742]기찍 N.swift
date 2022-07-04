675a02c67f7807e55805ee1c8b905d2080e764f7
PrismSpirit <yeolkeut@gmail.com>
2022-07-04 19:30:29 +0900

    Solve [2742]기찍 N

---
+++ Baekjoon/반복문/[2742]기찍 N.swift
@@ -0,0 +1,15 @@
+//
+//  [2742]기찍 N.swift
+//  Baekjoon
+//  https://www.acmicpc.net/problem/2742
+//
+//  Created by Jaehun Lee on 2022/07/04.
+//
+
+import Foundation
+
+var n = Int(readLine()!)!
+
+for i in (1...n).reversed() {
+    print(i)
+}
