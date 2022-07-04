5a4396ad55da1a5e5c2c423617ba19553e9c7266
PrismSpirit <yeolkeut@gmail.com>
2022-07-04 16:35:51 +0900

    Solve [1008]A/B

---
+++ Baekjoon/입출력과 사칙연산/[1008]AdivB.swift
@@ -0,0 +1,13 @@
+//
+//  [1008]AdivB.swift
+//  Baekjoon
+//  https://www.acmicpc.net/problem/1008
+//
+//  Created by Jaehun Lee on 2022/07/04.
+//
+
+import Foundation
+
+var input = readLine()!.components(separatedBy: " ").map { Double($0)! }
+
+print(input[0] / input[1])
