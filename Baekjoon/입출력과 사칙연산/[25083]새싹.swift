cc4fb8eaec226d74518b783cd65ae4a63b0d3765
PrismSpirit <yeolkeut@gmail.com>
2022-07-04 17:40:20 +0900

    Solve [25083]새싹

---
+++ Baekjoon/입출력과 사칙연산/[25083]새싹.swift
@@ -0,0 +1,16 @@
+//
+//  [25083]새싹.swift
+//  Baekjoon
+//  https://www.acmicpc.net/problem/25083
+//
+//  Created by Jaehun Lee on 2022/07/04.
+//
+
+import Foundation
+
+print("         ,r'\"7")
+print("r`-_   ,'  ,/")
+print(" \\. \". L_r'")
+print("   `~\\/")
+print("      |")
+print("      |")
