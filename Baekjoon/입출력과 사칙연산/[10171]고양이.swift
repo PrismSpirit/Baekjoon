7c8ce73e7a671c2859639a26c34a85c2dd0d43a5
PrismSpirit <yeolkeut@gmail.com>
2022-07-04 16:17:06 +0900

    Solve [10171]고양이

---
+++ Baekjoon/입출력과 사칙연산/[10171]고양이.swift
@@ -0,0 +1,14 @@
+//
+//  [10171]고양이.swift
+//  Baekjoon
+//  https://www.acmicpc.net/problem/10171
+//
+//  Created by Jaehun Lee on 2022/07/04.
+//
+
+import Foundation
+
+print("\\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \\(__)|")
