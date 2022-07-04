154d7a0342dabbcff3d768dd9ce719ac10fe5f46
PrismSpirit <yeolkeut@gmail.com>
2022-07-04 16:42:37 +0900

    Solve [10926]??!

---
+++ Baekjoon/입출력과 사칙연산/[10926]??!.swift
@@ -0,0 +1,13 @@
+//
+//  [10926]??!.swift
+//  Baekjoon
+//  https://www.acmicpc.net/problem/10926
+//
+//  Created by Jaehun Lee on 2022/07/04.
+//
+
+import Foundation
+
+var input = readLine()!
+
+print("\(input)??!")
