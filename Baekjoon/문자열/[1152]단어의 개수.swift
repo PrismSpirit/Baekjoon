d840092fe3f0fef7017617e5573520b47962b293
PrismSpirit <yeolkeut@gmail.com>
2022-07-05 19:36:49 +0900

    Solve [1152]단어의 개수

---
+++ Baekjoon/문자열/[1152]단어의 개수.swift
@@ -0,0 +1,11 @@
+//
+//  [1152]단어의 개수.swift
+//  Baekjoon
+//  https://www.acmicpc.net/problem/1152
+//
+//  Created by Jaehun Lee on 2022/07/05.
+//
+
+import Foundation
+
+print(readLine()!.split(separator: " ", omittingEmptySubsequences: true).count)
