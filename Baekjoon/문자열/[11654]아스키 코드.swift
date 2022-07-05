06dc67097a3e0b5a1bda4704159dc4fd2a70f1b6
PrismSpirit <yeolkeut@gmail.com>
2022-07-05 18:45:37 +0900

    Solve [11654]아스키 코드

---
+++ Baekjoon/문자열/[11654]아스키 코드.swift
@@ -0,0 +1,13 @@
+//
+//  [11654]아스키 코드.swift
+//  Baekjoon
+//  https://www.acmicpc.net/problem/11654
+//
+//  Created by Jaehun Lee on 2022/07/05.
+//
+
+import Foundation
+
+let c = Character(readLine()!)
+
+print(c)
